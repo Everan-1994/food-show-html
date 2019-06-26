cf7f0ca3d0df266b41ed43b4ebf89d5f4c66af71
malingling <2696386951@qq.com>
2019-06-26 20:35:43 +0800

    test

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,14 @@
 <template>
-	<div  class="min-width">
-		
+	<div class="min-width">
+
 		<nuxt />
-		
+
 	</div>
 </template>
 
 <script>
 	import Hearder from '~/components/header.vue';
-	
+
 	export default {
 
 		data() {
@@ -21,9 +21,23 @@
 		},
 		components: {
 			Hearder,
-			
+
 		},
-		methods: {}
+		mounted() {
+			this.getIsIe()
+		},
+		methods: {
+			getIsIe() {
+				if ('-ms-scroll-limit' in document.documentElement.style && '-ms-ime-align' in document.documentElement.style) {
+					window.addEventListener('hashchange', () => {
+						var currentPath = window.location.hash.slice(1)
+						if (this.$route.path !== currentPath) {
+							this.$router.push(currentPath)
+						}
+					}, false)
+				}
+			}
+		}
 	};
 </script>
 <style>
