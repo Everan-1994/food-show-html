2de324d9a9fe12b3284079cd8850331a27577262
malingling <2696386951@qq.com>
2019-06-26 20:28:37 +0800

    tets

--- pages/newTest.vue
+++ pages/newTest.vue
@@ -6,58 +6,7 @@
 				<h1 class="brand-title">关于我们</h1>
 				<p class="brand-desc">About us</p>
 
-				<div class="about-content">
-					<img src="~/assets/images/pic_about_leaf@2x.png" class="about-yezi" />
-					<div class="about-info-box">
-						<div :class="index == (i+1) ? 'about-info-list current' : 'about-info-list'" v-for="(item,i) in aboutArr.about_us"
-						 :key="i">
-							<h1>{{item.title}}</h1>
-							<div v-html="item.content"></div>
-
-							<div v-swiper:mySwiper="swiperOption" class="bannerss about-swiper" v-if="item.image.length>0">
-								<div class="swiper-wrapper">
-									<div class="swiper-slide" v-for="(item1,index) in item.image" :key="index">
-										<img :src="imgUrl+item1" />
-
-									</div>
-								</div>
-								<div class="swiper-pagination"></div>
-							</div>
-
-						</div>
-
-
-						<div :class="index==5 ?'about-info-list current':'about-info-list'">
-							<h1 class="common-problem">常见问题</h1>
-							<div class="problem-box" v-for="(item,index) in aboutArr.common_problem.data" :key="index" @click="getProblem(index)"
-							 :class="problemIndex==index ? 'problem-box open' : 'problem-box' ">
-								<div class="problem-name">
-									Q:{{item.question}}
-									<div class="problem-btn"></div>
-								</div>
-								<div class="problem-answer">
-									A:{{item.answer}}
-								</div>
-							</div>
-
-							<div class="page-box">
-								<div class="prev-box num-list" @click="getPrev"></div>
-								<div :class="page==count ? 'num-list active' : 'num-list' " v-for="count in aboutArr.common_problem.last_page"
-								 @click="getPage(count)">{{count}}</div>
-								<div class="next-box num-list" @click="getNext"></div>
-							</div>
-						</div>
-
-					</div>
-					<div class="about-title-box">
-						<div :class="index==1 ? 'about-title-list active' : 'about-title-list'" @click="change(1)">企业介绍</div>
-						<div :class="index==2 ? 'about-title-list active' : 'about-title-list'" @click="change(2)">董事长致辞</div>
-						<div :class="index==3 ? 'about-title-list active' : 'about-title-list'" @click="change(3)">企业文化</div>
-						<div :class="index==4 ? 'about-title-list active' : 'about-title-list'" @click="change(4)">企业理念</div>
-						<div :class="index==5 ? 'about-title-list active' : 'about-title-list'" @click="change(5)">常见问题</div>
-					</div>
-					<img src="../assets/images/pic_about_bg1@2x.png" class="about-posi2" />
-				</div>
+				
 
 			</div>
 
