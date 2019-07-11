986e900c0b28d9fbffdcea9553be02f97db59c10
malingling <2696386951@qq.com>
2019-07-11 21:28:12 +0800

    恢复配置文件

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,10 +11,10 @@ export default {
 		meta: [{
 				charset: 'utf-8'
 			},
-			// {
-			// 	name: 'viewport',
-			// 	// content: 'width=device-width, initial-scale=1'
-			// },
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1'
+			},
 			{
 				hid: 'description',
 				name: 'description',
