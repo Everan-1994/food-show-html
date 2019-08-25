2b3e6b63dea79bd7a795ba7a0884c5eb099d0989
everan-1994 <everan@aliyun.com>
2019-08-25 21:41:05 +0800

    修改路由格式

--- components/header.vue
+++ components/header.vue
@@ -3,13 +3,13 @@
 		<div class="container">
 			<ul class="nav">
 				<li :class="navActive == 'home' ? 'active' : ''">
-					<nuxt-link to="/" >首页</nuxt-link>
+					<nuxt-link to="/" no-prefetch>首页</nuxt-link>
 				</li>
 				<li :class="navActive == 'brand-cooperation' ? 'active' : ''">
 					<a href="/brand-cooperation">品牌合作</a>
 				</li>
 				<li :class="navActive == 'businessSuperCooperation' ? 'active' : ''">
-					<nuxt-link to="/businessSuperCooperation/default">商超合作</nuxt-link>
+					<nuxt-link to="/businessSuperCooperation/default" no-prefetch>商超合作</nuxt-link>
 				</li>
 				<li :class="navActive == 'brand' ? 'active' : ''">
 					<a href="/myBrand/brand">Lots of fun</a>
@@ -22,13 +22,13 @@
 			</div>
 			<ul class="nav right-nav">
 				<li :class="navActive == 'news' ? 'active' : ''">
-					<nuxt-link to="/news/0">新闻中心</nuxt-link>
+					<nuxt-link to="/news/0" no-prefetch>新闻中心</nuxt-link>
 				</li>
 				<li :class="navActive == 'about' ? 'active' : ''">
 					<a href="/aboutUs">关于我们</a>
 				</li>
 				<li :class="navActive == 'contact' ? 'active' : ''">
-					<nuxt-link to="/contact">联系我们</nuxt-link>
+					<nuxt-link to="/contact" no-prefetch>联系我们</nuxt-link>
 				</li>
 			</ul>
 		</div>
